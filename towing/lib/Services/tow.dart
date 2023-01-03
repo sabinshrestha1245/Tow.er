9fdc13b218e3c515426e509872af83b06612c5f4
Shrestha-sabin <shrestha.sabin.1245@gamil.com>
2023-01-03 23:41:08 +0545

    service and google map

--- towing/lib/Services/tow.dart
+++ towing/lib/Services/tow.dart
@@ -1,4 +1,6 @@
 import 'package:Tower/pages/googlemap.dart';
+import 'package:Tower/pages/homepage.dart';
+import 'package:Tower/pages/signuppage.dart';
 import 'package:flutter/material.dart';
 
 class Tow extends StatefulWidget {
@@ -11,67 +13,115 @@ class Tow extends StatefulWidget {
 class _TowState extends State<Tow> {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
+      body: SingleChildScrollView(
+        child: SafeArea(
           child: Column(
             children: [
-              Row(
-                //crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  //shedule booking
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    padding: EdgeInsets.all(12),
-
-                    child: Icon(
-                      Icons.calendar_today,
-                      color: Colors.black,
-                    ),
-                  )
-                ],
+          /*Container(
+          decoration: BoxDecoration(
+          color: Colors.cyan,
+              borderRadius: BorderRadius.circular(12)),
+          padding: EdgeInsets.all(12),
+          //alignment: Alignment.centerLeft,
+          child: TextField(
+            keyboardType: TextInputType.text,
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+              hintText: 'Set your Location',
+              labelText: 'Set Your Location',
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.black,
               ),
-              SizedBox(
-                height: 10,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.0),
+                //borderSide: const BorderSide(width: 0, style: BorderStyle.none)
               ),
-
-              //search bar
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.location_pin),
+              ),
+            ),
+          ),),*/
               Container(
-                decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.circular(12)),
-                padding: EdgeInsets.all(12),
-                //alignment: Alignment.centerLeft,
-                child: TextField(
-                  keyboardType: TextInputType.text,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Set your Location',
-                    labelText: 'Set Your Location',
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      //borderSide: const BorderSide(width: 0, style: BorderStyle.none)
-                    ),
-                    suffixIcon: IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.location_pin),
-                    ),
-                  ),
-                ),
+                  height: 800,
+              child: MapSample()
               ),
+
             ],
           ),
         ),
       ),
     );
+    /*Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  *//*Row(
+                    //crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      //shedule booking
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: EdgeInsets.all(12),
+
+                        child: Icon(
+                          Icons.calendar_today,
+                          color: Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+
+                  //search bar
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.cyan,
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.all(12),
+                    //alignment: Alignment.centerLeft,
+                    child: TextField(
+                      keyboardType: TextInputType.text,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'Set your Location',
+                        labelText: 'Set Your Location',
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          //borderSide: const BorderSide(width: 0, style: BorderStyle.none)
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.location_pin),
+                        ),
+                      ),
+                    ),
+                  ),*//*
+                  MapSample()
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );*/
   }
 }
