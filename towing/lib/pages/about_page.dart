6d7c7585d7efd8055c6f7df4279f68a6e186a50f
diwas2001 <suyaldiwas11@gmail.com>
2023-01-08 16:04:00 +0545

    Added About Page

---
+++ towing/lib/pages/about_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  State<AboutPage> createState() => AboutPageState();
+}
+
+class AboutPageState extends State<AboutPage
+> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("About Tow.Er : "
+            "\n Branch Head - Kathmandu \n"
+            "Diwas Suyal - 9823836514"),
+      ),
+    );
+  }
+}
--- towing/lib/pages/contact _information.dart
+++ towing/lib/pages/contact _information.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ContactInformation extends StatefulWidget {
   const ContactInformation({Key? key}) : super(key: key);
@@ -9,14 +10,83 @@ class ContactInformation extends StatefulWidget {
 }
 
 class _ContactInformationState extends State<ContactInformation> {
+  final TextEditingController text_controller = TextEditingController();
+  get body => null;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Contact the following branch : "
-            "\n Branch Head - Kathmandu \n"
-            "Diwas Suyal - 9823836514"),
-      ),
-      );
+    const maxLines = 15;
+    var size = MediaQuery.of(context).size;
+    return  Scaffold(
+        appBar: AppBar(
+          title:  Text("Contact Information",style: GoogleFonts.lato(
+              color: Colors.yellow, fontSize: 28,
+              backgroundColor: Colors.blue,fontWeight: FontWeight.bold
+          ),),
+          leading: const BackButton(
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:  [
+                    Center(
+                      child: Text('Share your issue',textAlign: TextAlign.center, style: GoogleFonts.lato(
+                        color: Colors.amber, fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                      ),),
+                    ),
+                    const SizedBox(height: 20,),
+                    Container(
+                      margin: const EdgeInsets.all(2.0),
+                      height: maxLines * 24.0,
+                      child: TextFormField(
+                        controller: text_controller ,
+                        maxLines: maxLines,
+                        keyboardType: TextInputType.multiline,
+                        decoration: const InputDecoration(
+                          hintText: "Enter your problem",
+                          filled: true,
+                          hintStyle: TextStyle(
+                              color: Colors.grey
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(18)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // SizedBox(height: 10,),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: (){}, child: Text(
+                        "Send ",style: GoogleFonts.lato(
+                        color: Colors.white,
+                      ), ),),
+                    ),
+                    Center(
+                      child: SizedBox(
+                        child: Row(
+                          children: [
+                            Text("Contact the following branch : "
+                              ,style: GoogleFonts.lato(
+                                color: Colors.black87,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ) );
   }
 }
