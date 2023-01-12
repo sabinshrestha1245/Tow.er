fd60a6df92b6426bc26e7c9ff3ab72891f0d5d78
Shrestha-sabin <shrestha.sabin.1245@gamil.com>
2023-01-13 05:23:44 +0545

    user details

--- towing/lib/customer_details.dart
+++ towing/lib/customer_details.dart
@@ -34,58 +34,71 @@ class _CustomerDetailsState extends State<CustomerDetails> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-
     return Scaffold(
-      body: Container(
-          width: size.width,
-          height: size.height,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Stack(
-
-              children: [Positioned(
-                 top: 50,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Name: ${loggedInUser.firstName} ${loggedInUser.lastName}",
-                      style: GoogleFonts.openSans(
-                          textStyle: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Email: ${loggedInUser.email}",
-                      style: GoogleFonts.openSans(
-                          textStyle: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Phone Number: ${loggedInUser.phone}",
-                      style: GoogleFonts.openSans(
-                          textStyle: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600)),
-                    ),
-                  ],
-                ),
-              ),]
-
-            ),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.white60,
+          elevation: 0,
+          title: const Text(
+            "User Details",
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
           ),
+        ),
+        body: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: SizedBox(
+                  child: Container(
+                    height: 400,
+                    width: 500,
+                    decoration: BoxDecoration(
+                      //color:  Colors.black,
+                      border: Border.all(
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Name: ${loggedInUser.firstName} ${loggedInUser.lastName}",
+                          style: GoogleFonts.openSans(
+                              textStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Email: ${loggedInUser.email}",
+                          style: GoogleFonts.openSans(
+                              textStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "Phone Number: ${loggedInUser.phone}",
+                          style: GoogleFonts.openSans(
+                              textStyle: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             )
 
         )
